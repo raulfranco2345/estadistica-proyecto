945f9530472b42cdff220371eb77c2c59d5852a7
Raul Franco <Panchulopez250590@email.com>
2026-04-12 20:08:14 -0600

    commit 10: mejoria de la interfaz y color

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ import numpy as np
 st.set_page_config(page_title="Estadística e IA - UP Chiapas", layout="wide")
 st.title("🚀 Proyecto Final: Análisis Estadístico e IA")
 st.caption("Autor: Franco Córdova Fabricio Raúl")
+# --- SIDEBAR: IDENTIDAD (Commit 9 y 10) ---
+with st.sidebar:
+    st.image("https://www.upchiapas.edu.mx/images/logo_up.png", width=200)
+    st.header("⚙️ Panel de Control")
+    st.divider()
+    st.subheader("🎨 Personalización")
+    color_graf = st.color_picker("Color de identidad visual:", "#3498db")
+    st.info("Autor: Franco Córdova Fabricio Raúl | ID: 253393")
 # --- MÓDULO 7: REFLEXIÓN (Commit 8) ---
 st.divider()
 st.header("7. Reflexión del Proceso Creativo")
